a5d64228d4a12106649da809f778155727fbd083
darkbeanbbq <darkbean01@gmail.com>
2013-09-12 10:30:39 +0200

    Added beta link on homepage + new alpha folder

---
+++ alpha/javascript.js
@@ -0,0 +1,29 @@
+/*function changeText()
+{
+document.getElementById("title").innerHTML='<h1 class="title">/exp</h1>'
+} */
+
+function linkedinText()
+{
+document.getElementById("title").innerHTML='/curriculum vitae'
+}
+
+function behanceText()
+{
+document.getElementById("title").innerHTML='/portfolio'
+}
+
+function tumblrText()
+{
+document.getElementById("title").innerHTML='/experiments'
+}
+
+function twitterText()
+{
+document.getElementById("title").innerHTML='/things I say'
+}
+
+function defaultText()
+{
+document.getElementById("title").innerHTML='darkbean.io'
+}
